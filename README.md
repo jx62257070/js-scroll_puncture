9b96a70e1d7905c94906f18105f3af1afa989217
chenshengjie <sj62257070>
2020-05-14 16:51:32 +0800

    优化

--- README.md
+++ README.md
@@ -8,15 +8,23 @@ cnpm install js-scroll_puncture
 
 ## Usage
 
+/**
+ * 方法说明
+ * @method openModal
+ * 
+ * @param {string} type 判断使用何种类型解决方案  "1":全局禁止默认点击事件(适用于弹框内无滚动要求)  "2".设置body.position和body.scrollTop(适用于弹框有滚轮要求)
+ * 
+ */
+
 ```javascript
 import {clickPop,closePop} from 'js-scroll_puncture'
 
-function openModal(){
+function openModal(type){
   clickPop()
   //do something
 }
 
-function closeModal(){
+function closeModal(type){
   closePop()
   //do something
 }
--- scrollPuncture.js
+++ scrollPuncture.js
@@ -1,12 +1,37 @@
 var nowScrollTop=0//初始化
 //禁止滚轮
-export function clickPop(){
-    nowScrollTop=document.body.scrollTop || document.documentElement.scrollTop //将页面的scroolTop赋值给变量
-    document.body.style.position="fixed"
-    document.body.style.top = -nowScrollTop + 'px'
+function touchStart_js_scroll_puncture(){
+    event.preventDefault();
+}
+export function clickPop(type){
+    var _type=type||"1"
+    switch (_type) {
+        case "1":
+            document.addEventListener('touchmove', touchStart_js_scroll_puncture,{passive:false});
+            break;
+        case "2":
+            nowScrollTop=document.body.scrollTop || document.documentElement.scrollTop //将页面的scroolTop赋值给变量
+            document.body.style.position="fixed"
+            document.body.style.top = -nowScrollTop + 'px'
+            break;
+        default:
+            break;
+    }
+    
 }
 //激活滚轮
-export function closePop(){
-    document.body.style.position="static"
-    document.body.scrollTop = document.documentElement.scrollTop = nowScrollTop
+export function closePop(type){
+    var _type=type||"1"
+    switch (_type) {
+        case "1":
+            document.removeEventListener('touchmove',touchStart,{passive:false});
+            break;
+        case "2":
+            document.body.style.position="static"
+            document.body.scrollTop = document.documentElement.scrollTop = nowScrollTop
+            break;
+        default:
+            break;
+    }
+    
 }
