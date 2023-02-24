e5ec4de8d2840d48143d008beb3d836de1aacff8
jx62257070 <42292902+jx62257070@users.noreply.github.com>
2023-02-24 06:53:04 +0000

    feat 适配pc滚动穿透

--- scrollPuncture.js
+++ scrollPuncture.js
@@ -1,37 +1,49 @@
-var nowScrollTop=0//初始化
+var nowScrollTop = 0//初始化
 //禁止滚轮
-function touchStart_js_scroll_puncture(){
+function touchStart_js_scroll_puncture() {
     event.preventDefault();
 }
-export function clickPop(type){
-    var _type=type||"1"
+export function clickPop(type) {
+    var _type = type || "1"
     switch (_type) {
         case "1":
-            document.addEventListener('touchmove', touchStart_js_scroll_puncture,{passive:false});
+            document.addEventListener('touchmove', touchStart_js_scroll_puncture, { passive: false });
+            //适配pc滚动事件
+            document.addEventListener('wheel', touchStart_js_scroll_puncture, { passive: false });
             break;
         case "2":
-            nowScrollTop=document.body.scrollTop || document.documentElement.scrollTop //将页面的scroolTop赋值给变量
-            document.body.style.position="fixed"
+            nowScrollTop = document.body.scrollTop || document.documentElement.scrollTop //将页面的scroolTop赋值给变量
+            document.body.style.position = "fixed"
             document.body.style.top = -nowScrollTop + 'px'
             break;
+        case '3':
+            //仅禁止pc滚动事件
+            document.addEventListener('wheel', touchStart_js_scroll_puncture, { passive: false });
+            break;
         default:
             break;
     }
-    
+
 }
 //激活滚轮
-export function closePop(type){
-    var _type=type||"1"
+export function closePop(type) {
+    var _type = type || "1"
     switch (_type) {
         case "1":
-            document.removeEventListener('touchmove',touchStart_js_scroll_puncture,{passive:false});
+            document.removeEventListener('touchmove', touchStart_js_scroll_puncture, { passive: false });
+            //适配pc滚动事件
+            document.removeEventListener('wheel', touchStart_js_scroll_puncture, { passive: false });
             break;
         case "2":
-            document.body.style.position="static"
+            document.body.style.position = "static"
             document.body.scrollTop = document.documentElement.scrollTop = nowScrollTop
+            break;
+        case '3':
+            //仅禁止pc滚动事件
+            document.removeEventListener('wheel', touchStart_js_scroll_puncture, { passive: false });
             break;
         default:
             break;
     }
-    
+
 }
